fc7a7a6b31315c835f695fefa681d17883331474
Alessandro Rebelo <96748455+whittejr@users.noreply.github.com>
2022-03-23 11:12:49 -0300

    pacote1

--- first/src/pacote1/entraDados.java
+++ first/src/pacote1/entraDados.java
@@ -1,7 +1,9 @@
+package pacote1;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class App
+public class entraDados
 {
     public static void main(String[] args) throws Exception 
     {
@@ -10,12 +12,16 @@ public class App
         Scanner sc = new Scanner(System.in);
 
         String s1, s2, s3;
-
+        int x;
+        
+        x = sc.nextInt();
+        sc.nextLine();
         s1 = sc.nextLine();
         s2 = sc.nextLine();
         s3 = sc.nextLine();
 
         System.out.println("Dados digitados: ");
+        System.out.println(x);
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
---
+++ first/src/pacote1/mathFunc.java
@@ -0,0 +1,6 @@
+package pacote1;
+
+public class mathFunc 
+{
+    
+}
